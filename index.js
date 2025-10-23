e6247c4ca8d81e32f2c6e4be75d9e991823da1da
Brendon W. <bwherry@midland.k12.ia.us>
2025-10-23 12:32:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+let addition = 50;
+addition =+ 5;
+
+let subtraction = 40;
+subtraction =- 1;
+
+let multiplication = 5;
+multiplication =* 7;
+
+let division = 6;
+division =/ 2;
+
+
+document.getElementById("result").innerHTML = multiplication;
+
+
+
+
+
+
+
+
+
+
+
+
 
